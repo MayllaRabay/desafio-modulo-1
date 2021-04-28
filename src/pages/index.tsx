2b2775b3ffced1aac7741955a844872e3d3a6392
MayllaRabay <mayllarabay@gmail.com>
2021-04-28 17:55:23 -0300

    Pasta 'Components' criada. Estilo global definido

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
 export default function Home() {
   return (
-    <h1>Hello World! Lets learn Next.js!</h1>
+    <>
+      <h1>Teste. aqui é um h1</h1>
+      <h3>Teste. aqui é um h3</h3>
+      <h5>Teste. aqui é um h5</h5>
+      <p>Aqui é um parágrafo</p>
+    </>
   )
 }
