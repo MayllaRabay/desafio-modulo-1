c13b8fb52ae469d6b05e493f2c28da1e5b467a1a
MayllaRabay <mayllarabay@gmail.com>
2021-05-01 20:23:54 -0300

    Pagina Contact feita

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,8 +2,64 @@ import styles from '../styles/contact.module.scss';
 
 export default function Contact() {
   return (
-    <div>
+    <div className={styles.contact__Container}>
+      <h1>Contato</h1>
 
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis maximus quis augue elementum vestibulum. Nam a 
+        orci erat. Aenean arcu mauris, finibus a rutrum quis, commodo sollicitudin nibh. Morbi diam lacus, suscipit 
+        nec maximus et, tincidunt at quam.
+      </p>
+
+      <h3>Informações para contato</h3>
+
+      <div>
+        <div className={styles.info}>
+          <img src="localizacao.png" alt="" /><strong>Endereço</strong>
+        </div>
+        <p>Av. Tancredo Neves, 2227, Ed. Salvador Prime Caminho das Árvores, Salvador • Bahia</p>
+      </div>
+
+      <div>
+        <div className={styles.info}>
+          <img src="telefone.png" alt="" /><strong>Telefone</strong>
+        </div>
+        <p>(71) 9 9999-9999 | (11) 1 1111-1111</p>
+      </div>
+
+      <div>
+        <div className={styles.info}>
+          <img src="headset.png" alt="" /><strong>Suporte</strong>
+        </div>
+        <p>suporte@cubos.academy</p>
+      </div>
+
+      <h3>Entre em contato</h3>
+
+      <form>
+        <div className={styles.form__Name__Email}>
+          <label>
+            <span>Nome</span>
+            <input id="name"></input>
+          </label>
+
+          <label>
+            <span>E-mail</span>
+            <input id="name" type="email"></input>
+          </label>
+        </div>
+
+        <label>
+          <span>Assunto</span>
+          <input id="name"></input>
+        </label>
+
+        <label>
+          <span>Comentário</span>
+          <textarea rows={10}></textarea>
+        </label>
+
+        <button>Enviar</button>
+      </form>
     </div>
   );
 }
